9398a991aa97dead843904227ff4d44502975bd0
saddemon1185 <148841848+saddemon1185@users.noreply.github.com>
2025-01-20 15:32:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~7qh0RDTJ#cCmnKt4FA2I80MkjuhPxbqf8ZKjVjaqKCJI3kFCUm8U",
 // add your Session Id 
 PREFIX: process.env.PREFIX || "+",
 // add your prifix for bot
